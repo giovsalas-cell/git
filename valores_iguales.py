cbc7055576d4c69ab4380495dd7bf89ae09aadbd
Giovanni <giovanni.salas26@gmail.com>
2026-02-24 21:50:32 -0300

    Mi primer commit

---
+++ valores_iguales.py
@@ -0,0 +1,20 @@
+
+
+def verificar_valores_iguales(array):
+        contador=0
+        arreglo_2=array
+        for a in range (0,len(arreglo_2)):
+                for b in range(0,len(array)):
+                        print(arreglo_2[b])
+                        
+                        
+                                
+        
+   
+
+arreglo=[1,2,3,4,1]
+
+verificar_valores_iguales(arreglo)
+
+
+
